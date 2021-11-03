0ca9e76ea8c2c2275bea35247577c9d65cacc198
Steph Heins <stephanie_heins@yahoo.com>
2021-11-02 20:48:37 -0700

    Added delete request to apiroutes, did some debugging for minor errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 // Dependencies 
 const fs = require("fs");
-var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+// var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+var data = require("../db/db.json")
 
 module.exports = function (app) {
 // API GET Request 
@@ -10,9 +11,34 @@ module.exports = function (app) {
     });
 
 // API POST Request 
+// Add saved note to db.json 
+    app.post("/api/notes", function (req, res) {
+        data.push(req.body);
 
+        // Assign ID 
+        data.forEach((obj, i) => {
+            obj.id = i + 1;
+        });
+
+        fs.writeFile("../db/db.json", JSON.stringify(data), function () {
+            res.json(data);
+        });
+    });
 
 // API DELETE Request 
+    app.delete("/api/notes/:id", function (req, res) {
+        var id = req.params.id;
 
+        // Delete selected note & reassign id
+        data.splice(id - 1, 1);
+        data.forEach((obj, i) => {
+        obj.id = i + 1;
+        });
+
+        // Return remaining notes 
+        fs.writeFile("../db/db.json", JSON.stringify(data), function () {
+        res.json(data);
+        });
+    });
 
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,8 +3,12 @@ const path = require("path");
 
 module.exports = function(app) {
 // HTML GET Request 
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-
-
-
-}
+  // Default to index.html if no matching routes 
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
