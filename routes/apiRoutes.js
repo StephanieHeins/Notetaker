a75b2ed36fa71fe4d17614461421782a4c2c043a
Steph Heins <stephanie_heins@yahoo.com>
2021-11-02 19:36:28 -0700

    Added some to apiroutes but unsure of a few things so leaving it there for now while i research

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,18 @@
 // Dependencies 
+const fs = require("fs");
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-
+module.exports = function (app) {
 // API GET Request 
-
+// Read db.json, return saved notes as json 
+    app.get("/api/notes", function(req, res) {
+        res.json(data);
+    });
 
 // API POST Request 
 
 
 // API DELETE Request 
+
+
+};
